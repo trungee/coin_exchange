0ff94f84b86adb8b134adebd56e61cc46c8beb82
trungnguyen <trungee.nguyen@gmail.com>
2021-03-12 07:29:11 +0700

    initial commit

---
+++ coinpricing/src/main/java/com/trungee/coinpricing/CoinpricingApplication.java
@@ -0,0 +1,13 @@
+package com.trungee.coinpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinpricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinpricingApplication.class, args);
+	}
+
+}
